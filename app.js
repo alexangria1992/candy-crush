cf56ed4a03246ca77fd1963462c45cab920daebf
Alexandre Angrignon <alex040892@gmail.com>
2024-02-26 17:49:41 +1300

    created  function to check for columns of three

--- app.js
+++ app.js
@@ -95,6 +95,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
   checkRowForThree();
 
+  // Check for column of three
+  function checkColumnForThree() {
+    for (i = 0; i < 47; i++) {
+      let columnOfThree = [i, i + width, i + width * 2];
+      let decidedColor = squares[i].style.backgroundColor;
+      const isBlank = squares[i].style.backgroundColor === "";
+
+      if (
+        columnOfThree.every(
+          (index) =>
+            squares[index].style.backgroundColor === decidedColor && !isBlank
+        )
+      ) {
+        score += 3;
+        columnOfThree.forEach((index) => {
+          squares[index].style.backgroundColor = "";
+        });
+      }
+    }
+  }
+
+  checkColumnForThree();
+
+  window.setInterval(function () {
+    checkRowForThree();
+    checkColumnForThree();
+  }, 100);
+
   function dragDrop() {
     console.log(this.id, "dragDrop");
     colorBeingReplaced = this.style.backgroundColor;
