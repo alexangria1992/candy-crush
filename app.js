f1ce99ab07eef9b84a44f0ef9609895fe8705f70
Alexandre Angrignon <alex040892@gmail.com>
2024-02-26 18:01:37 +1300

    functions that checks for row and column of 4

--- app.js
+++ app.js
@@ -73,11 +73,67 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Checking for matches
   // Check for row of three
+  function checkRowForFour() {
+    for (i = 0; i < 60; i++) {
+      let rowOfFour = [i, i + 1, i + 2, i + 3];
+      let decidedColor = squares[i].style.backgroundColor;
+      const isBlank = squares[i].style.backgroundColor === "";
+
+      const notValid = [
+        5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
+        54, 55,
+      ];
+      if (notValid.includes(i)) continue;
+
+      if (
+        rowOfFour.every(
+          (index) =>
+            squares[index].style.backgroundColor === decidedColor && !isBlank
+        )
+      ) {
+        score += 4;
+        rowOfFour.forEach((index) => {
+          squares[index].style.backgroundColor = "";
+        });
+      }
+    }
+  }
+
+  checkRowForFour();
+
+  // Check for column of three
+  function checkColumnForFour() {
+    for (i = 0; i < 47; i++) {
+      let columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      let decidedColor = squares[i].style.backgroundColor;
+      const isBlank = squares[i].style.backgroundColor === "";
+
+      if (
+        columnOfFour.every(
+          (index) =>
+            squares[index].style.backgroundColor === decidedColor && !isBlank
+        )
+      ) {
+        score += 4;
+        columnOfFour.forEach((index) => {
+          squares[index].style.backgroundColor = "";
+        });
+      }
+    }
+  }
+
+  checkColumnForFour();
+
+  // Checking for matches
+  // Check for row of three
   function checkRowForThree() {
     for (i = 0; i < 61; i++) {
       let rowOfThree = [i, i + 1, i + 2];
       let decidedColor = squares[i].style.backgroundColor;
       const isBlank = squares[i].style.backgroundColor === "";
+
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55];
+      if (notValid.includes(i)) continue;
 
       if (
         rowOfThree.every(
@@ -119,6 +175,8 @@ document.addEventListener("DOMContentLoaded", () => {
   checkColumnForThree();
 
   window.setInterval(function () {
+    checkRowForFour();
+    checkColumnForFour();
     checkRowForThree();
     checkColumnForThree();
   }, 100);
