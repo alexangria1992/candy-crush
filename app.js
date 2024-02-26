62c19f5a35ffb0b658475b51bc2f2f8cb902d620
Alexandre Angrignon <alex040892@gmail.com>
2024-02-26 17:32:46 +1300

    dragEnd function logic using an array, variable and if statement

--- app.js
+++ app.js
@@ -51,6 +51,23 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   function dragEnd() {
     console.log(this.id, "dragend");
+    // What is a valid move
+    let validMoves = [
+      squareIdBeingDragged - 1,
+      squareIdBeingDragged - width,
+      squareIdBeingDragged + 1,
+      squareIdBeingDragged + width,
+    ];
+    let validMove = validMoves.includes(squareIdBeingReplaced);
+
+    if (squareIdBeingReplaced && validMove) {
+      squareIdBeingReplaced = null;
+    } else if (squareIdBeingReplaced && !validMove) {
+      squares[squareIdBeingReplaced].style.backgroundColor = colorBeingReplaced;
+      squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged;
+    } else {
+      squares[squareIdBeingDragged].style.backgroundColor = colorBeingDragged;
+    }
   }
   function dragDrop() {
     console.log(this.id, "dragDrop");
